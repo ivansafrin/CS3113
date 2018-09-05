09544624db76a8d4ce651b6fca00ce60559a22b9
Ivan Safrin <ivansafrin@gmail.com>
2018-09-05 18:40:09 -0400

    Removed extra code from template

--- project_template/Windows/NYUCodebase/NYUCodebase/main.cpp
+++ project_template/Windows/NYUCodebase/NYUCodebase/main.cpp
@@ -4,13 +4,6 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
-#include <iostream>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#include "ShaderProgram.h"
-
-#include "glm/mat4x4.hpp"
-#include "glm/gtc/matrix_transform.hpp"
 
 #ifdef _WINDOWS
 #define RESOURCE_FOLDER ""
@@ -19,27 +12,6 @@
 #endif
 
 SDL_Window* displayWindow;
-
-GLuint LoadTexture(const char *filePath) {
-	int w,h,comp;
-	unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
-	
-	if(image == NULL) {
-		std::cout << "Unable to load image. Make sure the path is correct\n";
-		assert(false);
-	}
-	
-	GLuint retTexture;
-	glGenTextures(1, &retTexture);
-	glBindTexture(GL_TEXTURE_2D, retTexture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	
-	stbi_image_free(image);
-	return retTexture;
-}
 
 int main(int argc, char *argv[])
 {
